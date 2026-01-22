62b8ed03014a1e6fe02963b0c6d21d4ff94d0e67
Paulo Vitor <paulovitorpesoal@gmail.com>
2026-01-22 20:14:34 -0300

    responsive footer

--- src/app/diretivas/diretivas.footer.ts
+++ src/app/diretivas/diretivas.footer.ts
@@ -11,11 +11,22 @@ export class footerFundoDirective implements OnInit{
 
   ngOnInit(): void {
     //padrão de layout
-    this.el.style.backgroundColor = 'rgba(32, 32, 32, 1)';
-    this.el.style.height = '71px';
-    this.el.style.width = '872px';
+     this.el.style.backgroundColor = 'rgba(32, 32, 32, 1)';
+
+    this.el.style.width = '100%';
+    this.el.style.maxWidth = '872px';
+    this.el.style.minHeight = '71px';
+
+    this.el.style.boxSizing = 'border-box';
+
     this.el.style.border = '1px solid rgba(255, 255, 255, 0.2)';
     this.el.style.borderRadius = '8px';
+
+    // layout interno
+    this.el.style.display = 'flex';
+    this.el.style.alignItems = 'center';
+    this.el.style.gap = '16px';
+    this.el.style.padding = '16px 20px';
     
    
 
