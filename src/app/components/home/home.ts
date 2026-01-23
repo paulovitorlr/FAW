166315b6e16af9bbf7a84f91d8598a45de9401ec
Paulo Vitor <paulovitorpesoal@gmail.com>
2026-01-23 15:58:17 -0300

    realização de todo o responsive da pagina, adicionando a separacao de secao, site podendo ser usado para uso

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,17 +3,19 @@ import {divTransparentDirective} from 'S:/FAW_prj/FAW/src/app/diretivas/diretiva
 import {BtnGrupotDirective} from 'S:/FAW_prj/FAW/src/app/diretivas/button.grupo';
 import {aboutDirective} from 'S:/FAW_prj/FAW/src/app/diretivas/about';
 import {footerFundoDirective} from 'S:/FAW_prj/FAW/src/app/diretivas/diretivas.footer';
+
 @Component({
   selector: 'app-home',
   imports: [
     divTransparentDirective,
     BtnGrupotDirective,
     aboutDirective,
-    footerFundoDirective
+    footerFundoDirective,
+    
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  
 }
